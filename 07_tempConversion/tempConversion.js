ad11f0f42c507b878bbc2cc3153bcfd855f4baf2
ya-hs <68033139+ya-hs@users.noreply.github.com>
2023-12-21 12:11:36 +0000

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // (temp - 32) * 5/9
+  const convertedTemp = (temp - 32) * (5 / 9);
+  return Math.round(convertedTemp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // (temp * (9/5) + 32)
+  const convertedTemp = (temp * (9 / 5) + 32);
+  return Math.round(convertedTemp * 10) / 10;
 };
 
 // Do not edit below this line
