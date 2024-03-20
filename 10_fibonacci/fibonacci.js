f8f800e95e0d3c2bd0426e2477a21b3aadf42825
ya-hs <68033139+ya-hs@users.noreply.github.com>
2024-03-20 08:33:37 +0000

    complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
 const fibonacci = function (num) {
+    let oldVal = 0;
+    let curVal = 1;
+    let newVal
+    const seqNum = Number(num)
+    if (seqNum === 0) {
+        return oldVal;
+    } else if (seqNum < 0) {
+        return 'OOPS'
+    } else if (seqNum === 1) {
+        return curVal;
+    }
 
+    for (let i = 2; i <= seqNum; i++) {
+        newVal = oldVal + curVal;
+        oldVal = curVal;
+        curVal = newVal;
+    }
+    return newVal;
 };
 
 // Do not edit below this line
