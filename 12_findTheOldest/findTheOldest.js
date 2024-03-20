9e2c7b26e580ff21675b7f00038747de88be7d85
ya-hs <68033139+ya-hs@users.noreply.github.com>
2024-03-20 09:12:41 +0000

    complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    return array.reduce((oldPerson, currPerson) => {
+        const oldAge = getAge(oldPerson.yearOfBirth, oldPerson.yearOfDeath);
+        const youngAge = getAge(currPerson.yearOfBirth, currPerson.yearOfDeath);
+        return oldAge < youngAge ? currPerson : oldPerson;
+    });
 };
+
+const getAge = function (birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
